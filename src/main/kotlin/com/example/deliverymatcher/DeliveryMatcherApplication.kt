a0b3e1c4b8c408b59396299510ed6acf0699a237
chaesuhyeon <ddd8177@sweetk.co.kr>
2025-10-01 11:44:06 +0900

    :tada: create project

---
+++ src/main/kotlin/com/example/deliverymatcher/DeliveryMatcherApplication.kt
@@ -0,0 +1,11 @@
+package com.example.deliverymatcher
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DeliveryMatcherApplication
+
+fun main(args: Array<String>) {
+	runApplication<DeliveryMatcherApplication>(*args)
+}
